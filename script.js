a2756854b07e89d75858531bea0852f10097cc63
Alankar <alankarg701@gmail.com>
2026-02-05 19:47:22 +0530

    Update files

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-function returnbeforesubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate.value", inputdate.value); // string --> date (en_IN)
+let reCaptchacheck = false;
+function returnbeforesubmit(event) {
+  if (reCaptchacheck) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value", inputdate.value); // string --> date (en_IN)
 
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formattedDate;
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+  } else {
+    alert("Please verify that you are not a robot.");
+    event.preventDefault();
+  }
 }
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); 
-  if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value)
-    ;elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
-  } } setInterval(timestamp, 500);
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value,
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
+
+function captchasuccess() {
+  reCaptchacheck = true;
+}
